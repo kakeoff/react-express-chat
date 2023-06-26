b442525eb4cdb80886525a81d8d44e330027b863
Егор Пантелеев <maikelluri@icloud.com>
2023-06-26 23:22:30 +0300

    connecting sockets

--- client/src/components/Chat.jsx
+++ client/src/components/Chat.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
+import { useEffect } from 'react'
+import io from 'socket.io-client'
+import { useLocation } from 'react-router-dom'
+import { useState } from 'react'
 
+const socket = io.connect('http://localhost:4000')
 const Chat = () => {
-    return (
-        <div>Chat</div>
-    )
+  const { search } = useLocation()
+
+  const [params, setParams] = useState(null)
+
+  useEffect(() => {
+    const searchParams = Object.fromEntries(new URLSearchParams(search))
+
+    setParams(searchParams)
+
+    socket.emit('join', searchParams)
+  }, [search])
+
+  useEffect(() => {
+    socket.on('message', ({ data }) => {
+      console.log(data)
+    })
+  }, [])
+
+  console.log(params)
+  return <div>Chat</div>
 }
 
 export default Chat
--- server/index.js
+++ server/index.js
@@ -1,24 +1,43 @@
-const express = require('express');
-const http = require('http');
-const {Server} = require('socket.io');
-const cors = require('cors');
+const express = require('express')
+const http = require('http')
+const { Server } = require('socket.io')
+const cors = require('cors')
 
-const app = express();
+const app = express()
 
-const route = require('./route');
+const route = require('./route')
 
-app.use(cors({origin: '*'}));
-app.use(route);
+app.use(cors({ origin: '*' }))
+app.use(route)
 
-const server = http.createServer(app);
+const server = http.createServer(app)
 
 const io = new Server(server, {
   cors: {
     origin: '*',
     methods: ['GET', 'POST'],
   },
-});
+})
+
+io.on('connection', (socket) => {
+  socket.on('join', ({ name, room }) => {
+    socket.join(room)
+
+    socket.emit('message', {
+      data: {
+        user: {
+          name: 'admin',
+          message: `Hey, ${name}`,
+        },
+      },
+    })
+  })
+
+  io.on('disconnect', () => {
+    console.log('Disconnect')
+  })
+})
 
 server.listen(4000, () => {
-  console.log('Server is running');
-});
+  console.log('Server is running')
+})
